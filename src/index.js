db65c8a301415cea586fa6d10995a60abc26cd80
Andrew Gilbert <andrewg800@gmail.com>
2015-04-13 07:03:15 -0700

    Have JSON data get pulled from same server as page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var converter = new Showdown.converter();
 var QuestionViewer = React.createClass({
    loadCommentsFromServer: function() {
       $.ajax({
-         url: "http://andyg0808.github.io/TestRepo/" + this.props.url,
+         url: this.props.url,
          dataType: 'json',
          success: function(data) {
             this.setState({data: data});
@@ -146,7 +146,7 @@ var Question = React.createClass({
 
 $(function() {
    React.render(
-      <QuestionViewer url="data.json" pollInterval={2000} />,
+      <QuestionViewer url="/data.json" pollInterval={2000} />,
       document.getElementById('content')
    );
 })
